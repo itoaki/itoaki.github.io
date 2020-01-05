0f34f422e526e4167d095f28c01a82e0c3f1c1b3
ItoAkihiro <hatarakanai.ltd@gmail.com>
2020-01-05 18:02:17 +0900

    カードリスト作成

--- pages/accounts.vue
+++ pages/accounts.vue
@@ -9,84 +9,58 @@
       sm8
       md6
     >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          いろんなアカウントを並べるページ
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
+      <v-container fluid>
+        <v-card color="#292961" dark>
+          <v-card-title>
+            Account List
+          </v-card-title>
+        </v-card>
+
+        <v-row dense>
+          <v-col
+            v-for="card in cards"
+            :key="card.title"
+            :cols="card.flex"
+          >
+            <v-card
+              :href="card.href"
+              link
               target="_blank"
             >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
+              <v-img
+                :src="card.src"
+                class="white--text align-end"
+                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+                height="200px"
+              >
+                <v-card-title v-text="card.title" />
+              </v-img>
+              <v-card-text>
+                <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  data () {
+    return {
+      cards: [
+        { title: 'GitHub', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', href: 'https://github.com/itoaki', flex: 6 },
+        { title: 'GitLab', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', href: 'https://gitlab.com/Itoaki', flex: 6 },
+        { title: 'はてなブログ', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', href: 'https://techstation.hatenablog.com/', flex: 6 },
+        { title: 'note', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', href: 'https://note.com/itoaki0417', flex: 6 },
+        { title: 'Qiita', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', href: 'https://qiita.com/itoaki0417', flex: 6 },
+        { title: 'Twitter', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', href: 'https://twitter.com/itoaki0417', flex: 6 },
+        { title: 'Wantedly', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', href: 'https://www.wantedly.com/users/127449944', flex: 6 },
+        { title: 'Linkedin', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', href: 'https://www.linkedin.com/in/akihiro-ito-042319116', flex: 6 }
+      ]
+    }
   }
 }
 </script>
