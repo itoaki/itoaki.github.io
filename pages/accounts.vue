f181494af216161e3fa596984bed256aebc9da05
itoaki <itoaki.planet@gmail.com>
2020-01-11 11:15:40 +0900

    fix url

--- pages/accounts.vue
+++ pages/accounts.vue
@@ -65,49 +65,49 @@ export default {
         {
           title: 'GitHub',
           subtitle: 'GitHubのアカウントです。',
-          src: '/itoaki.github.io/logos/octocat.png',
+          src: '/logos/octocat.png',
           href: 'https://github.com/itoaki'
         },
         {
           title: 'GitLab',
           subtitle: 'GitLabのアカウントです。',
-          src: '/itoaki.github.io/logos/gitlab.svg',
+          src: '/logos/gitlab.svg',
           href: 'https://gitlab.com/Itoaki'
         },
         {
           title: 'はてなブログ',
           subtitle: '勉強したことをなんでも書いてます。',
-          src: '/itoaki.github.io/logos/hatenablog.svg',
+          src: '/logos/hatenablog.svg',
           href: 'https://techstation.hatenablog.com/'
         },
         {
           title: 'note',
           subtitle: '普段考えていることなどを書いてます。',
-          src: '/itoaki.github.io/logos/note.svg',
+          src: '/logos/note.svg',
           href: 'https://note.com/itoaki0417'
         },
         {
           title: 'Qiita',
           subtitle: '作成したもののまとめ記事を書いてます。',
-          src: '/itoaki.github.io/logos/qiita.png',
+          src: '/logos/qiita.png',
           href: 'https://qiita.com/itoaki0417'
         },
         {
           title: 'Twitter',
           subtitle: 'お気軽にフォローしてください。',
-          src: '/itoaki.github.io/logos/twitter.svg',
+          src: '/logos/twitter.svg',
           href: 'https://twitter.com/itoaki0417'
         },
         {
           title: 'Wantedly',
           subtitle: 'プロフィール鋭意記載中',
-          src: '/itoaki.github.io/logos/wantedly.svg',
+          src: '/logos/wantedly.svg',
           href: 'https://www.wantedly.com/users/127449944'
         },
         {
           title: 'Linkedin',
           subtitle: 'お友達になりましょう。',
-          src: '/itoaki.github.io/logos/linkedin.png',
+          src: '/logos/linkedin.png',
           href: 'https://www.linkedin.com/in/akihiro-ito-042319116'
         }
       ]
